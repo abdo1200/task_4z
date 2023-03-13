f2471dce042363841aaabfc4addf3226bc96ab13
abdo1200 <abdosobhy1200@gmail.com>
2023-03-13 20:34:46 +0200

    second

--- lib/controllers/post_controller.dart
+++ lib/controllers/post_controller.dart
@@ -1,31 +1,42 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:task_4z/model/post.dart';
 
 class PostController extends GetxController {
   var isLoading = false.obs;
-  RxBool isConnected = false.obs;
+  RxString isConnected = 'offline'.obs;
+  Timer? timer;
 
   List<Child>? posts;
+  List<Child>? offlinePosts;
   @override
   Future onInit() async {
     super.onInit();
-    checkConnection();
+    timer = Timer.periodic(
+        const Duration(seconds: 1), (Timer t) => checkConnection());
     fetchData();
   }
 
-  Future<bool> checkConnection() async {
+  @override
+  void dispose() {
+    super.dispose();
+    timer?.cancel();
+  }
+
+  checkConnection() async {
     final connectivityResult = await Connectivity().checkConnectivity();
-    print(connectivityResult);
+
     if (connectivityResult == ConnectivityResult.wifi ||
         connectivityResult == ConnectivityResult.mobile) {
-      isConnected = true.obs;
+      isConnected.value = 'online';
     } else {
-      isConnected = false.obs;
+      isConnected.value = 'offline';
     }
-    return isConnected.value;
+    isConnected.refresh;
   }
 
   fetchData() async {
@@ -36,8 +47,30 @@ class PostController extends GetxController {
       if (response.statusCode == 200) {
         var result = jsonDecode(response.body);
         posts = PostModel.fromJson(result).data.children;
+      }
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+
+      // Map<String, dynamic> postsModel = ;
+
+      // bool result = await prefs.setString('user', jsonEncode(user));
+    } catch (e) {
+      //print(e);
+    } finally {
+      isLoading(false);
+    }
+  }
+
+  fetchDataOffline() async {
+    try {
+      SharedPreferences shared_User = await SharedPreferences.getInstance();
+      if (shared_User.getString('posts')!.isEmpty) {
+        print('empty');
       } else {
-        //error
+        print('done');
+        Map<String, dynamic> postsData =
+            jsonDecode(shared_User.getString('posts')!);
+        offlinePosts = PostModel.fromJson(postsData).data.children;
+        print(offlinePosts);
       }
     } catch (e) {
       //print(e);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -22,104 +22,66 @@ class Home extends StatelessWidget {
             'assets/reddit-logo.svg',
             height: 80,
           ),
-          FutureBuilder<bool>(
-              future: controller.checkConnection(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  if (snapshot.data!) {
-                    return Obx(
-                      () => controller.isLoading.value
-                          ? SizedBox(
-                              height: Get.height * .7,
-                              child: Center(
-                                child: LoadingAnimationWidget.discreteCircle(
-                                  color: Colors.white,
-                                  size: 50,
-                                ),
-                              ),
-                            )
-                          : Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: ListView.builder(
-                                  itemCount: controller.posts!.length,
-                                  itemBuilder: (context, index) {
-                                    return GestureDetector(
-                                      onTap: () => Get.to(() => PostDetails(
-                                          post: controller.posts![index].data)),
-                                      child: PostCard(
-                                          author: controller
-                                              .posts![index].data.author,
-                                          title: controller
-                                              .posts![index].data.title),
-                                    );
-                                  },
-                                ),
-                              ),
-                            ),
-                    );
-                  } else {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: Get.width,
-                          alignment: Alignment.center,
-                          margin: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              color: Colors.red,
-                              borderRadius: BorderRadius.circular(20),
-                              boxShadow: const [
-                                BoxShadow(
-                                    color: Colors.black,
-                                    blurRadius: 2,
-                                    spreadRadius: .5)
-                              ]),
-                          child: const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text('Please check your internet connection',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Obx(() =>
+                  Text('Active statu : ${controller.isConnected.value}   ')),
+              Obx(() => controller.isConnected.value == 'online'
+                  ? Container(
+                      height: 15,
+                      width: 15,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(50)),
+                    )
+                  : Container(
+                      height: 15,
+                      width: 15,
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(50)),
+                    )),
+            ],
+          ),
+          Obx(
+            () => controller.isConnected.value == "online"
+                ? controller.isLoading.value
+                    ? SizedBox(
+                        height: Get.height * .7,
+                        child: Center(
+                          child: LoadingAnimationWidget.discreteCircle(
+                            color: Colors.white,
+                            size: 50,
                           ),
                         ),
-                        GestureDetector(
-                          onTap: () async {
-                            await controller.checkConnection();
-                            if (controller.isConnected.value) {
-                              await controller.fetchData();
-                              Get.to(() => Home());
-                            }
-                          },
-                          child: Container(
-                              padding: const EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(20),
-                                  boxShadow: const [
-                                    BoxShadow(
-                                        color: Colors.black,
-                                        blurRadius: 2,
-                                        spreadRadius: .5)
-                                  ]),
-                              child: const Text('Try Again',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.amber))),
-                        )
-                      ],
-                    );
-                  }
-                } else {
-                  return SizedBox(
-                    height: Get.height * .7,
-                    child: Center(
-                      child: LoadingAnimationWidget.discreteCircle(
-                        color: Colors.white,
-                        size: 50,
-                      ),
-                    ),
-                  );
-                }
-              })
+                      )
+                    : Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListView.builder(
+                            itemCount: controller.posts!.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () => Get.to(() => PostDetails(
+                                    post: controller.posts![index].data)),
+                                child: PostCard(
+                                    author:
+                                        controller.posts![index].data.author,
+                                    title: controller.posts![index].data.title),
+                              );
+                            },
+                          ),
+                        ),
+                      )
+                : Container(
+                    margin: const EdgeInsets.only(top: 40),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Text('Check your internet connection')),
+          )
         ],
       )),
     );
